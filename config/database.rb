f93ec257f57d83bad090bb3c5e122fca82fc314a
toticavalcanti <toticavalcanti@hotmail.com>
2018-07-13 15:34:30 -0300

    create and set chat_bot/Rackfile

--- config/database.rb
+++ config/database.rb
@@ -2,7 +2,7 @@ configure :test do
    set :database, {
      adapter: 'postgresql',
      encoding: 'utf8',
-     database: 'onebitbot_test',
+     database: 'chat_bot_test',
      pool: 5,
      username: 'postgres',
      host: 'postgres'
@@ -13,7 +13,7 @@ configure :development do
  set :database, {
    adapter: 'postgresql',
    encoding: 'utf8',
-   database: 'onebitbot_development',
+   database: 'chat_bot_development',
    pool: 5,
    username: 'postgres',
    host: 'postgres'
@@ -24,7 +24,7 @@ configure :production do
    set :database, {
      adapter: 'postgresql',
      encoding: 'utf8',
-     database: 'onebitbot_production',
+     database: 'chat_bot_production',
      pool: 5,
      username: 'postgres',
      host: 'postgres'
