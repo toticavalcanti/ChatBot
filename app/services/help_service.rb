6c3dfc03cd81e2d28554699cbcb5446bc1718387
toticavalcanti <toticavalcanti@hotmail.com>
2018-07-26 16:58:57 -0300

    put response in line 16 of app/services/help_service.rb

--- app/services/help_service.rb
+++ app/services/help_service.rb
@@ -13,5 +13,6 @@ class HelpService
       response += ">Lista as perguntas e respostas com aquela hashtag\n\n"
       response += "*Perguntas e Respostas*\n"
       response += ">Mostra a lista de perguntas e respostas\n\n"
+      response
     end
   end
