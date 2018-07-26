609caaca82730c6c040ebfda5bccb2caa26975ba
toticavalcanti <toticavalcanti@hotmail.com>
2018-07-26 16:42:34 -0300

    fix line 5 in app/models/hashtag.rb

--- app/models/hashtag.rb
+++ app/models/hashtag.rb
@@ -2,5 +2,5 @@ class Hashtag < ActiveRecord::Base
   validates_presence_of :name
   belongs_to :company
   has_many :faq_hashtags
-  has_many :hashtags, through: :faq_hashtags
+  has_many :faqs, through: :faq_hashtags
 end
