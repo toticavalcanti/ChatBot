7a3f410c1e50d9cf6dbef95bf4e4bef09a41fb6a
toticavalcanti <toticavalcanti@hotmail.com>
2018-07-27 11:20:18 -0300

    update app.rb

--- app.rb
+++ app.rb
@@ -5,13 +5,27 @@ require 'sinatra/activerecord'
 require './config/database'
 
 Dir["./app/models/*.rb"].each {|file| require file }
+Dir["./app/services/**/*.rb"].each {|file| require file }
 
 class App < Sinatra::Base
+
   get '/sinatra' do
-    'Hello world Sinatra!'
+    'Hello world Sinatra!'   
   end
 
-  # post '/webhook' do
-  #
-  # end
+  post '/webhook' do
+    request.body.rewind
+    result = JSON.parse(request.body.read)["queryResult"]
+
+    if result["contexts"].present?
+      response = InterpretService.call(result["action"], result["contexts"][0]["parameters"])
+    else
+      response = InterpretService.call(result["action"], result["parameters"])
+    end
+
+    content_type :json, charset: 'utf-8'
+    {
+      "fulfillmentText": response
+    }.to_json
+  end
 end
